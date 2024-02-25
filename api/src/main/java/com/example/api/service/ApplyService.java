893431c31f61f01d2a029edb71541c28a067c1dc
yuhwanwoo <6514687@naver.com>
2024-02-25 13:59:42 +0900

    [first-come] redis를 통한 개선

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,20 +1,24 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplyService {
     private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     public void apply(Long userId) {
-        long count = couponRepository.count();
-
+        // redis incr key:value 1씩 증가 시킴
+        long count = couponCountRepository.increment();
+        System.out.println("count = " + count);
         if (count > 100) {
             return;
         }
